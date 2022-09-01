23492177f1d1841b1930cf09a89497e94f6d8440
Huzaifa <huzaifar@digitaiz.com>
2022-09-01 22:41:41 +0500

    edit and update functionality

--- app/controllers/portfolios_controller.rb
+++ app/controllers/portfolios_controller.rb
@@ -19,9 +19,22 @@ class PortfoliosController < ApplicationController
     end
   end
 
-  def show
-    
+  def edit
+    @portfolio = Portfolio.find(params[:id])
   end
+  
+  def update
+    @portfolio = Portfolio.find(params[:id])
+    respond_to do |format|
+      if @portfolio.update(portfolio_params)
+        format.html { redirect_to portfolios_path(@portfolio), notice: "Portfolio was successfully updated." }
+      else
+        format.html { render :edit, status: :unprocessable_entity }
+      end
+    end
+  end
+
+
   
   
   private
