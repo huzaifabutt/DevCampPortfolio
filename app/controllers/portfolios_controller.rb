5be3d507c4e50e7ca0fe244bb13ec353dec6f797
Huzaifa <huzaifar@digitaiz.com>
2022-09-01 07:21:40 +0500

    integrate new and create actions for portfolio

--- app/controllers/portfolios_controller.rb
+++ app/controllers/portfolios_controller.rb
@@ -2,4 +2,30 @@ class PortfoliosController < ApplicationController
   def index
     @portfolios = Portfolio.all
   end
+
+  def new
+    @portfolio = Portfolio.new
+  end
+  
+  def create
+    @portfolio = Portfolio.new(portfolio_params)
+
+    respond_to do |format|
+      if @portfolio.save
+        format.html { redirect_to portfolios_path(@portfolio), notice: "Portfolio was successfully created." }
+      else
+        format.html { render :new, status: :unprocessable_entity }
+      end
+    end
+  end
+
+  def show
+    
+  end
+  
+  
+  private
+  def portfolio_params
+    params.require(:portfolio).permit(:title,:subtitle, :body)
+  end
 end
